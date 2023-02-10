8eb7ddae28db15361435427652a4a59d65d8debd
MD Naushad <100362851+MD07Naushad@users.noreply.github.com>
2023-02-10 09:43:46 +0530

    Update 192. find XOR of number from L to R.java

--- 192. find XOR of number from L to R.java
+++ 192. find XOR of number from L to R.java
@@ -1,1 +1,25 @@
-
+/*package whatever //do not write package name here */
+ 
+import java.io.*;
+ 
+class GFG {
+    // Function to return the XOR of elements
+    // from the range [l, r]
+    public static int findXOR(int l, int r)
+    {
+        int ans = 0;
+        for (int i = l; i <= r; i++) {
+            ans = ans ^ i;
+        }
+        return ans;
+    }
+ 
+    // Driver code
+    public static void main(String[] args)
+    {
+        int l = 4;
+        int r = 8;
+ 
+        System.out.println(findXOR(l, r));
+    }
+}
